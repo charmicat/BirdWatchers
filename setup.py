50b167ed20ef0281ffa2c4eaddb692527a7b5663
Luiza Utsch <charmi.cat@gmail.com>
2017-06-14 23:49:26 +0100

    changes for openshift

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='BirdsWatcher',
+    version='1.0',
+    packages=['vag'],
+    url='https://birdswatcher-vag.rhcloud.com/',
+    license='',
+    author='Luiza Utsch',
+    author_email='',
+    description=''
+)
--- vag/FollowersCheck.py
+++ vag/FollowersCheck.py
@@ -10,7 +10,7 @@ import hashlib
 import os
 
 
-class UnfollowCheck(object):
+class FollowersCheck(object):
     '''
     classdocs
     '''
